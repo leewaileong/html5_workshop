ed7282b1bdf2f0ea5e5581e0efa2402551ea1929
issuser <leewaileong@gmail.com>
2018-09-04 17:25:13 +0800

    ver 1

---
+++ workshop01/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { LineItem } from './model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  cart: LineItem[] = [];
+
+  processLineItem(lineItem: LineItem){
+    this.cart.push(lineItem);
+    console.log("cart = ",this.cart);
+  }
+
+  title = 'workshop01';
+}
